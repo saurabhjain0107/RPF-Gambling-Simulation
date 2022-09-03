82b0ad1da27ea400fe4bd13268c69618a46540da
saurabh jain <luckyjain0107@gmai.com>
2022-09-03 20:22:34 +0530

    50% of the stake or Would resign for the Day

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -4,21 +4,26 @@ public class GamblingSimulator {
 
     public static void main(String[] args) {
         System.out.println("welcome to Gambling Simulation");
+        int totalWinningAmount=0;
         for (int day = 1; day <= 20; day++) {
             int dayStake=100;
+            int winningAmount=0;
             System.out.println("#day"+day);
-            dayStake+=doBet(1);
+            while (dayStake<150 && dayStake>50){
+                dayStake+=doBet(1);
+            }
+            winningAmount+=(dayStake-100);
+            totalWinningAmount+=dayStake;
+            System.out.println("resigned the game");
             day++;
         }
     }
-        static int doBet(int bid){
-            int random=(int)(Math.random()*10)%2;
-            if (random == 1) {
-                System.out.println("you won 1$");
-                return 1;
-            }else{
-                System.out.println("you loose 1$");
-                return -1;
-            }
+    static int doBet(int bid){
+        int random=(int)(Math.random()*10)%2;
+        if (random == 1) {
+            return 1;
+        }else{
+            return -1;
+        }
     }
 }
