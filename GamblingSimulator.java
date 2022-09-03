ae0b1a24c28b5e8798786c5608a4b91a6c25e501
saurabh jain <luckyjain0107@gmai.com>
2022-09-03 20:10:16 +0530

    Start position

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
+
     public static void main(String[] args) {
         System.out.println("welcome to Gambling Simulation");
+        for (int day = 1; day <= 20; day++) {
+            int dayStake = 100;
+            doBet(1);
+        }
+    }
+        static int doBet(int bid){
+
+        return 0;
     }
 }
