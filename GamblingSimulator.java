9b5b25833bfa51e11053133b73fb9a780cf7d002
saurabh jain <luckyjain0107@gmai.com>
2022-09-03 20:17:22 +0530

    either win or loose

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -5,12 +5,20 @@ public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("welcome to Gambling Simulation");
         for (int day = 1; day <= 20; day++) {
-            int dayStake = 100;
-            doBet(1);
+            int dayStake=100;
+            System.out.println("#day"+day);
+            dayStake+=doBet(1);
+            day++;
         }
     }
         static int doBet(int bid){
-
-        return 0;
+            int random=(int)(Math.random()*10)%2;
+            if (random == 1) {
+                System.out.println("you won 1$");
+                return 1;
+            }else{
+                System.out.println("you loose 1$");
+                return -1;
+            }
     }
 }
